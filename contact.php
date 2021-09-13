9b65a68707da5a5c5251b8af08fac4ab8ced9150
Asadullah Galib <md.asadullah196@gmail.com>
2021-09-13 19:48:10 +0600

    Todays game started now! Working on email notification for the users

--- contact.php
+++ contact.php
@@ -28,46 +28,46 @@ if (isset($_POST['submit'])) {
 	<br><br><br><br>
 	<section>
 		<div class="container">
-				<div class="row">
-					<div class="col-xl-4">
-						<div class="company_address">
-							<h2>Hospital Address :</h2>
-							<br><br>
-							<p>Plot # 9/2, Avenue # 5,</p>
-							<p>Section # 6, Block # B,</p>
-							<p>Mirpur, Dhaka</p>
-							<p>Phone:(00) 222 666 444</p>
-							<p>Fax: (000) 000 00 00 0</p>
-							<p>Email: <span>info@galibnotes.com</span></p>
-						</div>
-					</div>
-					<div class="col-xl-8">
-						<div class="contact-form">
-							<h2>Contact Us</h2>
-							<form name="contactus" method="post">
-								<div>
-									<span><label>NAME</label></span>
-									<span><input type="text" name="fullname" required="true" value=""></span>
-								</div>
-								<div>
-									<span><label>E-MAIL</label></span>
-									<span><input type="email" name="emailid" required="ture" value=""></span>
-								</div>
-								<div>
-									<span><label>MOBILE NO</label></span>
-									<span><input type="text" name="mobileno" required="true" value=""></span>
-								</div>
-								<div>
-									<span><label>Description</label></span>
-									<span><textarea name="description" required="true"> </textarea></span>
-								</div>
-								<div>
-									<span><input type="submit" name="submit" value="Submit"></span>
-								</div>
-							</form>
-						</div>
+			<div class="row">
+				<div class="col-xl-4">
+					<div class="company_address">
+						<h2>Hospital Address :</h2>
+						<br><br>
+						<p>Plot # 9/2, Avenue # 5,</p>
+						<p>Section # 6, Block # B,</p>
+						<p>Mirpur, Dhaka</p>
+						<p>Phone:(00) 222 666 444</p>
+						<p>Fax: (000) 000 00 00 0</p>
+						<p>Email: <span>info@galibnotes.com</span></p>
 					</div>
 				</div>
+				<div class="col-xl-8">
+					<div class="contact-form">
+						<h2>Contact Us</h2>
+						<form name="contactus" method="post">
+							<div>
+								<span><label>NAME</label></span>
+								<span><input type="text" name="fullname" required="true" value=""></span>
+							</div>
+							<div>
+								<span><label>E-MAIL</label></span>
+								<span><input type="email" name="emailid" required="ture" value=""></span>
+							</div>
+							<div>
+								<span><label>MOBILE NO</label></span>
+								<span><input type="text" name="mobileno" required="true" value=""></span>
+							</div>
+							<div>
+								<span><label>Description</label></span>
+								<span><textarea name="description" required="true"> </textarea></span>
+							</div>
+							<div>
+								<span><input type="submit" name="submit" value="Submit"></span>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 	<br><br>
