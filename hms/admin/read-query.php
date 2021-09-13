ddcb7d7e125c2639b9e11e5add3331a57d2686e8
Asadullah Galib <md.asadullah196@gmail.com>
2021-09-13 23:56:34 +0600

    Dlete column added

--- hms/admin/read-query.php
+++ hms/admin/read-query.php
@@ -75,7 +75,8 @@ if (isset($_GET['del'])) {
 											<th class="hidden-xs">Email</th>
 											<th>Contact No. </th>
 											<th>Message </th>
-											<th>Action</th>
+											<th>View</th>
+											<th>Delete</th>
 
 										</tr>
 									</thead>
@@ -97,29 +98,10 @@ if (isset($_GET['del'])) {
 													<div class="visible-md visible-lg hidden-sm hidden-xs">
 														<a href="query-details.php?id=<?php echo $row['id']; ?>" class="btn btn-transparent btn-lg" title="View Details"><i class="fa fa-file"></i></a>
 													</div>
-													<div class="visible-xs visible-sm hidden-md hidden-lg">
-														<div class="btn-group" dropdown is-open="status.isopen">
-															<button type="button" class="btn btn-primary btn-o btn-sm dropdown-toggle" dropdown-toggle>
-																<i class="fa fa-cog"></i>&nbsp;<span class="caret"></span>
-															</button>
-															<ul class="dropdown-menu pull-right dropdown-light" role="menu">
-																<li>
-																	<a href="#">
-																		Edit
-																	</a>
-																</li>
-																<li>
-																	<a href="#">
-																		Share
-																	</a>
-																</li>
-																<li>
-																	<a href="#">
-																		Remove
-																	</a>
-																</li>
-															</ul>
-														</div>
+												</td>
+												<td>
+													<div class="visible-md visible-lg hidden-sm hidden-xs">
+														<a href="query-details.php?id=<?php echo $row['id']; ?>" class="btn btn-transparent btn-lg" title="View Details"><i class="fa fa-file"></i></a>
 													</div>
 												</td>
 											</tr>
